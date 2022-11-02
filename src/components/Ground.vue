4c1da5b65493bf4b21fc07cb8649ee186f70509c
JADE PARK <dev.jadepark@gmail.com>
2022-11-02 23:46:16 +0900

    memoApp, express

--- src/components/Ground.vue
+++ src/components/Ground.vue
@@ -10,6 +10,7 @@
 import Header from "./Header.vue"
 import Footer from "./Footer.vue"
 import Memo from "./Memo.vue"
+
 export default {
   components:{Header, Footer, Memo}
 }
--- src/components/Memo.vue
+++ src/components/Memo.vue
@@ -1,3 +1,56 @@
 <template>
-  <memo>I am Memo</memo>
+  <div class="memo">
+    <div class="act">
+      <button class="btn btn-primary" @click="add()">+ 추가</button>
+    </div>
+    <ul>
+      <li v-for="(d, idx) in state.data" :key="idx">{{ d }}</li>
+    </ul>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.memo {
+  .act {
+    padding: 10px 10px 5px 5px;
+    text-align: right;
+  }
+  ul {
+    border: 1px solid #eee;
+    list-style: none;
+    padding: 15px 0;
+    margin: 0;
+
+    li {
+      padding: px 10px;
+      margin: 5px;
+      border: 1px solid #eee;
+    }
+  }
+}
+</style>
+
+<script>
+import { reactive } from 'vue';
+import axios from "axios";
+
+export default {
+  setup() {
+    const state = reactive({
+      data: [],
+    })
+
+    const add = () => {
+      state.data.push("추가된 메모 내용");
+    }
+
+    axios.get("/api/memos").then((res) => {
+      console.log(res.data);
+      state.data = res.data;
+    })
+    
+
+    return { state, add };
+  },
+};
+</script>
